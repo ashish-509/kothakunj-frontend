4f15733d4af30ab14752891f4eb28722b28f8f08
ashish-509 <ashish.078bei006@tcioe.edu.np>
2024-08-09 16:45:20 +0545

    final commit changing profile and register

--- app/(auth)/(routes)/signup/page.jsx
+++ app/(auth)/(routes)/signup/page.jsx
@@ -1,19 +1,69 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import axios from "axios";
 
 export default function Register() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    address: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.password !== formData.confirmPassword) {
+      setErrorMessage("Passwords do not match!");
+      return;
+    }
+
+    axios
+      .post("https://kothakunj-backend-1.onrender.com/register", {
+        first_name: formData.firstName,
+        last_name: formData.lastName,
+        phone: formData.phone,
+        address: formData.address,
+        email: formData.email,
+        password: formData.password,
+      })
+      .then((response) => {
+        console.log("User registered successfully:", response.data);
+        // Handle successful registration (e.g., redirect or display success message)
+      })
+      .catch((error) => {
+        console.error("There was an error registering the user!", error);
+        // Handle registration error (e.g., display error message)
+      });
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-purple-100 text-black">
       <div className="bg-white p-6 rounded-md shadow-md w-3/5 h-5/6">
         <h2 className="text-2xl font-semibold mb-8">Register</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="grid grid-cols-2 gap-8 mb-6">
             <div className="col-span-1">
               <label className="block text-gray-700 mb-2">First Name</label>
               <input
                 type="text"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
                 className="w-full px-3 py-3 border border-gray-300 rounded text-black"
                 placeholder="First Name"
               />
@@ -22,6 +72,9 @@ export default function Register() {
               <label className="block text-gray-700 mb-2">Last Name</label>
               <input
                 type="text"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
                 className="w-full px-3 py-3 border border-gray-300 rounded text-black"
                 placeholder="Last Name"
               />
@@ -32,6 +85,9 @@ export default function Register() {
               <label className="block text-gray-700 mb-2">Mobile</label>
               <input
                 type="tel"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
                 className="w-full px-3 py-3 border border-gray-300 rounded text-black"
                 placeholder="Mobile"
               />
@@ -40,6 +96,9 @@ export default function Register() {
               <label className="block text-gray-700 mb-2">Address</label>
               <input
                 type="text"
+                name="address"
+                value={formData.address}
+                onChange={handleChange}
                 className="w-full px-3 py-3 border border-gray-300 rounded text-black"
                 placeholder="Address"
               />
@@ -49,6 +108,9 @@ export default function Register() {
             <label className="block text-gray-700 mb-2">Email</label>
             <input
               type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
               className="w-full px-3 py-3 border border-gray-300 rounded text-black"
               placeholder="Email"
             />
@@ -58,6 +120,9 @@ export default function Register() {
               <label className="block text-gray-700 mb-2">Password</label>
               <input
                 type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
                 className="w-full px-3 py-3 border border-gray-300 rounded text-black"
                 placeholder="Password"
               />
@@ -68,11 +133,17 @@ export default function Register() {
               </label>
               <input
                 type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
                 className="w-full px-3 py-3 border border-gray-300 rounded text-black"
                 placeholder="Confirm Password"
               />
             </div>
           </div>
+          {errorMessage && (
+            <p className="text-red-500 text-sm mb-4">{errorMessage}</p>
+          )}
           <div className="mt-8">
             <button
               type="submit"
--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -1,61 +1,54 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import Image from "next/image";
 import { useTheme } from "../lib/ThemeContext";
 
 const ProfileCard = ({
-  name,
-  category,
+  first_name,
+  last_name,
+  phone_number,
+  address,
+  email,
   photo,
-  description,
-  reviews,
-  reviewAuthor,
-  rating,
 }) => {
   const { theme } = useTheme();
 
   return (
-    <div className={`pb-11 pt-11 ${theme === "dark" ? "bg-black text-white" : "bg-custom-gray text-black"}`}>
-      <div className="max-w-md mx-auto p-6 rounded-lg">
+    <div
+      className={`pb-11 pt-11 ${
+        theme === "dark" ? "bg-black text-white" : "bg-custom-gray text-black"
+      }`}
+    >
+      <div className="max-w-md mx-auto p-6 rounded-lg shadow-md">
         <div className="flex justify-center mb-4">
-          <div className="w-24 h-24 bg-gray-300 rounded-full flex items-center justify-center overflow-hidden">
+          <div className="w-24 h-24 bg-gray-300 rounded-full overflow-hidden">
             {photo ? (
-              <img
+              <Image
                 src={photo}
-                alt="Profile"
-                className="w-full h-full object-cover"
+                alt={`${first_name} ${last_name} Profile Picture`}
+                layout="responsive"
+                width={96}
+                height={96}
+                className="rounded-full object-cover"
+                priority
               />
             ) : (
-              <span>Photo</span>
+              <div className="w-full h-full flex items-center justify-center">
+                <span className="text-gray-500">No Photo</span>
+              </div>
             )}
           </div>
         </div>
         <div className="text-center mb-4">
-          <h2 className="text-2xl font-bold mb-1">{name}</h2>
-          <p className="text-lg text-green-500">{category}</p>
+          <h2 className="text-2xl font-bold mb-1">
+            {first_name} {last_name}
+          </h2>
+          <p className="text-lg text-green-500">{email}</p>
         </div>
         <div className="mb-6 text-center">
-          <h3 className="text-xl font-semibold mb-2">Description</h3>
-          <p className="text-center">{description}</p>
-        </div>
-        <div className="text-center">
-          <h3 className="text-xl font-semibold mb-2">Reviews</h3>
-          <div className="flex justify-center items-center mb-2">
-            {Array.from({ length: 5 }, (_, index) => (
-              <span
-                key={index}
-                className={`text-yellow-400 ${
-                  index < rating ? "text-yellow-400" : "text-gray-500"
-                }`}
-              >
-                {index < rating ? "★" : "☆"}
-              </span>
-            ))}
-          </div>
-          <p className="text-center">
-            <span className="text-white-800 font-bold">{reviewAuthor}</span>
-            {" -- "}
-            {reviews}
-          </p>
+          <h3 className="text-xl font-semibold mb-2">Contact Information</h3>
+          <p className="text-center">Phone: {phone_number}</p>
+          <p className="text-center">Address: {address}</p>
         </div>
       </div>
     </div>
@@ -63,13 +56,46 @@ const ProfileCard = ({
 };
 
 ProfileCard.propTypes = {
-  name: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
+  first_name: PropTypes.string.isRequired,
+  last_name: PropTypes.string.isRequired,
+  phone_number: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
   photo: PropTypes.string,
-  description: PropTypes.string.isRequired,
-  reviews: PropTypes.string.isRequired,
-  reviewAuthor: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
 };
+
+ProfileCard.defaultProps = {
+  photo: null,
+};
+
+export async function getServerSideProps() {
+  try {
+    const res = await fetch(
+      "https://kothakunj-backend-1.onrender.com/api/user"
+    );
+    if (!res.ok) {
+      throw new Error(`Failed to fetch user data: ${res.statusText}`);
+    }
+
+    const data = await res.json();
+
+    return {
+      props: {
+        first_name: data.first_name || "First Name",
+        last_name: data.last_name || "Last Name",
+        phone_number: data.phone_number || "No Phone Number",
+        address: data.address || "No Address",
+        email: data.email || "No Email",
+        photo: data.photo || null,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+
+    return {
+      notFound: true,
+    };
+  }
+}
 
 export default ProfileCard;
